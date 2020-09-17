d02c648fe80f06e265fdecd491a10d8b0fca958a
David Rouw <david_rouw@hotmail.com>
2020-09-17 15:12:21 +0200

    Finished lab 6

--- src/main/java/LabsEnHwOpdrachten/lab6/Lab6.java
+++ src/main/java/LabsEnHwOpdrachten/lab6/Lab6.java
@@ -1,19 +1,22 @@
 package LabsEnHwOpdrachten.lab6;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Lab6 {
 
     public static void main(String[] args) {
         Lab6 l = new Lab6();
-        l.calculateFromInput(8);
+        //l.calculateFromInput(8);
+        l.randomCube(6);
     }
 
-    public int[] calculateFromInput(int numberAmt){
+    public int[] calculateFromInput(int numberAmt) {
         int counter = 0;
         int[] storage = new int[numberAmt];
         int sum = 0;
-        while (counter < numberAmt){
+        while (counter < numberAmt) {
             Scanner in = new Scanner(System.in);
             System.out.println("Enter a number..");
             String numString = in.nextLine();
@@ -23,11 +26,37 @@ public class Lab6 {
             counter++;
         }
         System.out.print("You have given the following numbers: ");
-        for (int i : storage){
+        for (int i : storage) {
             System.out.print(i + " ");
         }
         System.out.println();
         System.out.println("It totals to " + sum);
         return storage;
+    }
+
+
+    public void randomCube(int size) {
+        Random rand = new Random();
+        int[][] cube = new int[size][size];
+        ArrayList<Integer> numbersUsed = new ArrayList<Integer>();
+        int newNumber = 0; //starting value, need to be initialized
+        numbersUsed.add(0); //0 can never be in the cube
+        for (int rij = 0; rij < size; rij++) {
+            for (int kol = 0; kol < size; kol++) {
+                while (numbersUsed.contains(newNumber)){
+                    newNumber = rand.nextInt((size*size)+1);
+                }
+                cube[rij][kol] = newNumber;
+                numbersUsed.add(newNumber);
+            }
+        }
+
+        //for printing the cube
+        for (int[] row : cube) {
+            for (int num : row) {
+                System.out.printf("%-3s",num);
+            }
+            System.out.println();
+        }
     }
 }
