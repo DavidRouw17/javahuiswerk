a641efc6f30c12eb5174969f0cb4c15f2d46f651
David Rouw <david_rouw@hotmail.com>
2020-09-29 14:48:05 +0200

    Now with JUnit 5!

--- labs/src/test/java/LabsEnHwOpdrachten/lab11/BankTest.java
+++ labs/src/test/java/LabsEnHwOpdrachten/lab11/BankTest.java
@@ -3,15 +3,15 @@ package LabsEnHwOpdrachten.lab11;
 
 
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BankTest {
 
     private Bank bank;
+
     @BeforeEach
     public void setUp() {
         bank = new Bank();
@@ -19,14 +19,28 @@ public class BankTest {
         bank.addCheckingAccount(300); //2
         bank.addSavingsAccount(0.5); //3
         bank.addSavingsAccount(1.5, 300); //4
+    }
 
+
+    @Test
+    public void testIfAddingAccountWorks() {
+        bank.addCheckingAccount();
+        //bank.printAccounts();
+
+        assertEquals(bank.bankSize(), 5);
     }
 
     @Test
-    public void testIfAddingAccountWorks(){
-        bank.addCheckingAccount();
+    public void testIfAddingMoneyWorks() {
+        bank.getAccount(4).deposit(250);
+
+        assertEquals(550, bank.getAccount(4).getBalance());
+    }
+
+    @Test
+    public void testIfWithdrawingMoneyWorks() {
+        bank.getAccount(2).withdraw(137);
+        assertEquals(163, bank.getAccount(2).getBalance());
         bank.printAccounts();
-
-
     }
 }
